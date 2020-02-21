98b9ad63c1f84f62fcbe69787f6103c08d0774df
MateuVieira <mateus12v@hotmail.com>
2020-02-21 20:52:14 -0300

    Add config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+
+import './config/ReactotronConfig';
+import Routes from './routes';
+import history from './services/history';
+
+import GlobalStyled from './styles/global';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <Router history={history}>
+      <Routes />
+      <GlobalStyled />
+    </Router>
+  );
 }
 
 export default App;
